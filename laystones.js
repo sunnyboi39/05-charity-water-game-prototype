534931d58463d0130b76987ead96c1d3ca332491
sunday smith <smiths109@montclair.edu>
2026-07-20 04:30:34 +0000

    image adjusment to grid

--- laystones.js
+++ laystones.js
@@ -7,16 +7,21 @@ let spaces=[[false,false,false,false,false,false],[false,false,false,false,false
 function makeStone(stone){
   const imgWrap=document.createElement("div");
   const stoneImg=document.createElement("img");
+  imgWrap.style.width = "100%";
+  imgWrap.style.height = "100%";
 
   if(stone==stoneType[0]){
     stoneImg.src="img/Lstone.png";
     stoneImg.alt = 'l shape stone';
+    stoneImg.style.width = "100%";
+    stoneImg.style.height = "100%";
   };
   /* add more elifs*/
   imgWrap.appendChild(stoneImg);
   well.appendChild(imgWrap);
   imgWrap.style.zIndex = "10"; 
   imgWrap.style.gridRow = "1 / 3";
+  imgWrap.style.gridColumn = "1 / 4";
   imgWrap.style.position = "absolute";
 
 };
